9c996b140483db47268a8244ad605cb9c9713e87
Siripireddy, Hemanth <Hemanth.Siripireddy@verint.com>
2025-02-21 18:23:09 +0530

    sectin20

---
+++ section20/eazy-bom/common/src/main/java/com/eazybytes/common/dto/CommonTestConfig.java
@@ -0,0 +1,9 @@
+package com.eazybytes.common.dto;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.eazybytes.common") // Adjust the package as needed
+public class CommonTestConfig {
+}
---
+++ section20/eazy-bom/common/src/test/java/com/eazybytes/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eazybytes.common;
+
+import com.eazybytes.common.dto.CommonTestConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CommonTestConfig.class)
+public class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
